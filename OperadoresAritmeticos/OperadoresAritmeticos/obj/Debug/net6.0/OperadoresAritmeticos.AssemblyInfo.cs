c14014418019bdebf2efeb7bd8b67ceb774521a7
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-01-15 10:03:44 -0600

    Add files via upload

--- OperadoresAritmeticos/OperadoresAritmeticos/obj/Debug/net6.0/OperadoresAritmeticos.AssemblyInfo.cs
+++ OperadoresAritmeticos/OperadoresAritmeticos/obj/Debug/net6.0/OperadoresAritmeticos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperadoresAritmeticos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e283a1c2483bb1d26094d922b599a9b3f311665")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26b24905355c9c0f87f586a4fa1ebea31728ff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperadoresAritmeticos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperadoresAritmeticos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
