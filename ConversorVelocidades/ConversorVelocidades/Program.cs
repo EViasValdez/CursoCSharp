47bd7eb2bd4978df2e890ac3d3cb1b7b76940c37
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-04-22 22:14:31 -0600

    Add files via upload

--- ConversorVelocidades/ConversorVelocidades/Program.cs
+++ ConversorVelocidades/ConversorVelocidades/Program.cs
@@ -8,48 +8,48 @@ namespace ConversorVelocidades
 
         static void Main()
         {
-            int opcion, repetir = 0;
-            var con = new Conversor();
+            int Opcion, Repetir = 0;
+            var Con = new Conversor();
             
             do
             {
                 Console.WriteLine("Escoja una de las opciones");
                 Console.WriteLine("1 - Convertir m/s a km/h");
                 Console.WriteLine("2 - Convertir km/h a m/s");
-                opcion = Convert.ToInt32(Console.ReadLine());
-                switch (opcion)
+                Opcion = Convert.ToInt32(Console.ReadLine());
+                switch (Opcion)
                 {
                     case 1:
-                        con.Conversor1();
+                        Con.Conversor1();
                         break;
                     case 2:
-                        con.Conversor2();
+                        Con.Conversor2();
                         break;
                     default:
                         Console.WriteLine("Opción errónea");
                         break;
                 }
                 Console.WriteLine("Si desea repetir coloque el valor de 1 o cualquier tecla para salir");
-                repetir = Convert.ToInt32(Console.ReadLine());
-            } while (repetir == 1);
+                Repetir = Convert.ToInt32(Console.ReadLine());
+            } while (Repetir == 1);
 
         }
         class Conversor
         {
 
-            private double velocidad;
+            private double Velocidad;
 
             public void Conversor1()
             {
-                Console.WriteLine("Introduzca una velocidad en m/s");
-                velocidad = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine($"{velocidad} m/s = {velocidad * 3600 / 1000} km/h");
+                Console.WriteLine("Introduzca una Velocidad en m/s");
+                Velocidad = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"{Velocidad} m/s = {Velocidad * 3600 / 1000} km/h");
             }
             public void Conversor2()
             {
-                Console.WriteLine("Introduzca una velocidad en km/h");
-                velocidad = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine($"{velocidad} km/h = {velocidad * 1000 / 3600} m/s");
+                Console.WriteLine("Introduzca una Velocidad en km/h");
+                Velocidad = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"{Velocidad} km/h = {Velocidad * 1000 / 3600} m/s");
             }
         }
     }
